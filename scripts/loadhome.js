b153623fa4ca44cab2a3e82a41426c585ea6433d
samarth <30378006+samarth1989@users.noreply.github.com>
2021-03-07 21:20:10 +0000

    home-page and details-page changes

---
+++ scripts/loadhome.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+        var jsonData = '';
+        // FETCHING jsonData FROM JSON FILE 
+        $.getJSON("Property_Complete.json",
+            function (jsonData) {
+                //debugger;
+                //console.log(jsonjsonData);
+
+    for (var i = 0; i < jsonData.length; i++) {
+        // var type = "";
+        // if (jsonData[i].data.type == 2) {
+        //     type = " Sale ";
+        // }
+        // else {
+        //     type = " Rent ";
+        // }
+        //debugger;
+        //console.log(jsonData[i].data.images[0]);
+        var alt = 'Address: ' + jsonData[i].data.address + "<br />" + ' County: ' + jsonData[i].data.county + ' Area: ' + jsonData[i].data.area + "<br />";
+        var alt1 = jsonData[i].data.numBedroom + 'BHK' + ' For ' + 'type' + 'Price: ' + jsonData[i].data.maxPrice;
+        var mhtml = "";
+
+        mhtml += '<div class=""><a href="housedetails.html?id=' + jsonData[i].id + '"><img src="' + jsonData[i].data.photo[0] + '" href="HouseDetails.html?id=' + jsonData[i].id + '" width="300" height="300"/></a></div>';
+        mhtml += '<a href="HouseDetails.html?id=' + jsonData[i].id + '" style="color:blue;font-weight:15pt;">' + alt + '</a>';
+        mhtml += '<a href="HouseDetails.html?id=' + jsonData[i].id + '">' + alt1 + '</a>';
+
+        console.log(mhtml)
+        $('#mydiv').append(mhtml);
+    }
+})
+});
+
+
---
+++ scripts/loadhousedetails.js
@@ -0,0 +1,48 @@
+
+
+        var propId = getParameterByName('id');
+        //console.log(propId);
+
+        $(document).ready(function () {
+
+            var dataJson = '';
+            // FETCHING DATA FROM JSON FILE 
+            $.getJSON("Property_Complete.json",
+                function (data) {
+                    debugger;
+                    dataJson = data[propId].data;
+                    //console.log(dataJson);
+                    
+                    //displayPropDetail(dataJson);
+                    var alt = dataJson.listingName + "<br />" + ' County: ' + dataJson.county + ' Area: ' + dataJson.area + "<br />";
+                    var alt1 = dataJson.numBedroom + 'BHK' + ' For ' + 'Price: ' + dataJson.maxPrice;
+                    var mhtml = "";
+
+                    for (var i = 0; i < dataJson.photo.length; i++) {		
+                        mhtml += '<div class=""><img src="' + dataJson.photo[i] + '" width="auto" height="auto"/></div>';
+                    }
+
+                    mhtml += '<p>' + dataJson.description + '</p>';
+
+                    var mhtml1 = '<p>' + alt + '</p>';
+                    mhtml1 += '<p>' + alt1 + '</p>';
+                    mhtml1 += '<p>' + dataJson.address + '</p>';
+                    
+                    //console.log(mhtml)
+                    $('#c123').append(mhtml);
+                    $('#c1234').append(mhtml1);
+
+
+
+                })
+        });
+        function getParameterByName(name, url = window.location.href) {
+
+            //capture the query string params
+            name = name.replace(/[\[\]]/g, '\\$&');
+            var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+                results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return (decodeURIComponent(results[2].replace(/\+/g, ' ')));
+        }
